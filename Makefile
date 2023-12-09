b27741f9f9eccaea8a5d55d3f81ab681593f7269
Zack Bostock <zbostock56@gmail.com>
2023-12-08 21:08:45 -0500

    Added basic print functionality, updates to Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,23 @@
 ASM=nasm
 
 SRC_DIR=src
-BUILD_DIR=build
+BUILD_DIR=./build
+EXEC=main_floppy.img
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
-	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+.PHONY: run clean
+
+$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)  $(BUILD_DIR)/main.bin	
+	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/$(EXEC)
+	truncate -s 1440k $(BUILD_DIR)/$(EXEC)
 
 $(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
 	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+
+$(BUILD_DIR):  
+	mkdir -p $(BUILD_DIR)
+
+run:
+	qemu-system-i386 -fda $(BUILD_DIR)/$(EXEC)
+
+clean:
+	rm -rf $(BUILD_DIR)
--- src/main.asm
+++ src/main.asm
@@ -4,11 +4,57 @@ org 0x7C00
 ; 16 bit output from assembler
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+  jmp main
+
+; ---------- print ----------
+; Output character(s) to screen
+; Input: ds:si
+
+print:
+  push si
+  push ax
+
+.loop:
+  lodsb         ; Loads character
+  or al, al     ; Is the character null?
+  jz .endloop
+
+  mov ah, 0x0e  ; Call bios interrupt
+  mov bh, 0     ; Set page number to zero
+  int 0x10
+
+  jmp .loop
+
+.endloop:
+  pop ax
+  pop si
+  ret
+; --------------------------
+
 main:
+  ; Set up data segments
+  mov ax, 0
+  mov ds, ax
+  mov es, ax
+
+  ; Set up stack pointer and stack segment
+  mov ss, ax
+  mov sp, 0x7C00
+
+  ; Print test message
+  mov si, test_msg
+  call print
+
   hlt
 .halt:
   jmp .halt
 
+test_msg: db 'Hello world!', ENDL, 0 
+
 ; Tell nasm to put 0xAA55 at the end of the file
+; to indicate this is the boot sector
 times 510-($-$$) db 0
 dw 0AA55h
