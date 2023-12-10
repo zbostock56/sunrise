1ca4412cfeb2d3c0ec540eaf86cc5cc2f8aab655
Zack Bostock <zbostock56@gmail.com>
2023-12-10 00:39:52 -0500

    Added disk reading from FAT12, added toy kernel for testing, changes to Makefile

--- Makefile
+++ Makefile
@@ -3,16 +3,42 @@ ASM=nasm
 SRC_DIR=src
 BUILD_DIR=./build
 EXEC=main_floppy.img
+BL=bootloader.bin
+KERNEL=kernel.bin
+KERNEL_DIR=kernel
 
-.PHONY: run clean
+.PHONY: run clean kernel bootloader floppy_img bd
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)  $(BUILD_DIR)/main.bin	
-	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/$(EXEC)
-	truncate -s 1440k $(BUILD_DIR)/$(EXEC)
 
-$(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
-	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+floppy_img: $(BUILD_DIR)/$(EXEC)
+kernel: $(BUILD_DIR)/$(KERNEL)
+bootloader: $(BUILD_DIR)/$(BL)
+bd: $(BUILD_DIR)
 
+#
+# Floppy Image
+#
+$(BUILD_DIR)/$(EXEC): bootloader kernel 
+	dd if=/dev/zero of=$(BUILD_DIR)/$(EXEC) bs=512 count=2880
+	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/$(EXEC)
+	dd if=$(BUILD_DIR)/$(BL) of=$(BUILD_DIR)/$(EXEC) conv=notrunc
+	mcopy -i $(BUILD_DIR)/$(EXEC) $(BUILD_DIR)/$(KERNEL) "::kernel.bin"
+
+#
+# Test Kernel
+#
+$(BUILD_DIR)/$(KERNEL): bd
+	$(ASM) $(KERNEL_DIR)/main.asm -f bin -o $(BUILD_DIR)/$(KERNEL)
+
+#
+# Bootloader
+#
+$(BUILD_DIR)/$(BL): bd 
+	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/$(BL)
+
+#
+# Build Directory
+#
 $(BUILD_DIR):  
 	mkdir -p $(BUILD_DIR)
 
--- src/main.asm
+++ src/main.asm
@@ -6,8 +6,46 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
+;
+; ---------- FAT12 Headers ----------
+;
+jmp short start 
+nop
+
+; BIOS Parameter Block
+bpb_oem:                      db 'MSWIN4.1'
+bpb_bytes_per_sector:         dw 512
+bpb_sectors_per_cluster:      db 1
+bpb_reserved_sectors:         dw 1
+bpb_fat_count:                db 2
+bpb_dir_entries_count:        dw 0E0h
+bpb_total_sectors:            dw 2880  ; 2880 * 512 = 1.44 MB
+bpb_media_descriptor:         db 0F0h  ; 0xF0 = 3.5" floppy
+bpb_sectors_per_fat:          dw 9
+bpb_sectors_per_track:        dw 18
+bpb_heads:                    dw 2
+bpb_hidden_sectors:           dd 0
+bpb_large_sector_count:       dd 0
+
+; Extended Boot Record
+ebr_drive_number:             db 0
+db 0                                             ; Implicitly reserved byte
+ebr_signiture:                db 28h             ; Serial number, value doesn't matter
+ebr_volume_id:                db 01h, 02h, 03h, 04h
+ebr_volume_label:             db 'sunrise_bl '   ; Must be 11 bytes
+ebr_system_id:                db 'FAT12   '      ; Must be 8 bytes 
+; ------------------------------
+
+
+;
+; Entry point from BIOS
+;
 start:
   jmp main
+
+;
+; Helper Routines
+;
 
 ; ---------- print ----------
 ; Output character(s) to screen
@@ -34,6 +72,126 @@ print:
   ret
 ; --------------------------
 
+;
+; Disk I/O Routines
+;
+
+; ---------- lba_to_chs ----------
+; Output: 
+;   - cx [bits 0 - 5]: sector number
+;   - cx [bits 6 - 15]: cylinder
+;   - dh: head
+; Input: 
+;   - LBA address (ax) 
+; Desc:
+;   Sector = (LBA % sectors_per_track) + 1
+;   Head = (LBA / sectors_per_track) % heads
+;   cylinder = (LBA / sectors_per_track) / heads
+
+lba_to_chs:
+  push ax
+  push dx
+
+  xor dx, dx                        ; Set dx to zero
+  div word [bpb_sectors_per_track]  ; ax = LBA / sectors_per_track
+                                    ; dx = remainer of division
+
+  inc dx                            ; Perform increment as described in desc
+  mov cx, dx
+
+  xor dx, dx                        ; Set dx to zero
+  div word [bpb_heads]              ; ax = (LBA / sectors_per_track) / heads = cylinder
+                                    ; dx = (LBA / sectors_per_track) % heads = head
+  mov dh, dl                        ; move lower bits of ax (signified by dl)
+  mov ch, al                        ; ch = cylinder (lower 8 bits)
+                                    ; Next three operations set bits as described in desc
+  shl ah, 6
+  or cl, ah
+
+  pop ax
+  mov dl, al                        ; Restore dl
+  pop ax
+  ret
+; --------------------------------
+
+; ---------- disk_read ----------
+; Input:
+;   - ax: LBA address
+;   - cl: Number of sectors to read (up to 128)
+;   - dl: Drive number
+;   - es:bx: Location to store the data read
+
+disk_read:
+  push ax
+  push bx
+  push cx
+  push dx
+  push di
+
+  push cx                      ; Temporarily save cl (number of sectors to read)
+  call lba_to_chs
+  pop ax                       ; al = number of sectors to read
+
+  mov ah, 02h
+  mov di, 3                    ; Retry count (floppies are unreliable)
+
+.loop:
+  pusha                        ; Not sure what registers BIOS interrupt will use
+  stc                          ; Set carry flag (some BIOS's don't do it)
+  int 13h 
+  jnc .success                 ; If carry flag is cleared, operation successful
+  popa 
+  call disk_reset              ; Reset the floppy controller
+  dec di 
+  test di, di                  ; Test loop condition
+  jnz .loop
+
+.fail:
+  ; All booting attempts failed
+  jmp floppy_error
+
+.success:
+  popa
+  pop di
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+; -------------------------------
+
+; ---------- disk_reset -----------
+; Input:
+;   - dl: drive_number
+disk_reset: 
+  pusha
+  mov ah, 0
+  stc
+  int 13h
+  jc floppy_error
+  popa
+  ret
+
+; --------------------------------
+
+
+;
+; Error Handlers
+;
+floppy_error:
+  mov si, read_failed_msg
+  call print
+  jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+  mov ah, 0
+  int 16h                     ; Await keypress
+  jmp 0FFFFh:0                ; Jump to beginning of bios (should reboot system)
+
+
+;
+; Main function
+;
 main:
   ; Set up data segments
   mov ax, 0
@@ -44,15 +202,24 @@ main:
   mov ss, ax
   mov sp, 0x7C00
 
+  ; Test read from floppy
+  mov [ebr_drive_number], dl
+
+  mov ax, 1
+  mov cl, 1
+  mov bx, 0x7E00
+  call disk_read
+
   ; Print test message
-  mov si, test_msg
+  mov si, welcome_msg
   call print
 
+  cli
   hlt
-.halt:
-  jmp .halt
 
-test_msg: db 'Hello world!', ENDL, 0 
+
+welcome_msg: db 'Welcome to sunrise bootloader!', ENDL, 0 
+read_failed_msg: db 'Floppy read error!', ENDL, 0
 
 ; Tell nasm to put 0xAA55 at the end of the file
 ; to indicate this is the boot sector
